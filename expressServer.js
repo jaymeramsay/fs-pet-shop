8b552a87a1fdc2d7bb5f7fe93a991733a797faa2
Jayme Ramsay <jayme.ramsay@gmail.com>
2018-01-28 23:44:08 -0600

    modifies package.json

--- expressServer.js
+++ expressServer.js
@@ -1,16 +1,21 @@
 'use strict';
-const fs = require('fs');
+let fs = require('fs');
 const path = require('path');
 const petsPath = path.join(__dirname, 'pets.json');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 8002;
 
 app.disable('x-powered-by');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 app.get('/pets', function (req, res) {
-  let petData = 'pets.json';
   fs.readFile(petsPath, 'utf8', function (err, petData) {
     if (err) {
       console.error(err.stack);
@@ -19,11 +24,11 @@ app.get('/pets', function (req, res) {
 
     let pets = JSON.parse(petData);
 
-    res.send(petData);
+    res.json(pets);
   });
 });
 
-app.get('/pets:id', function (req, res) {
+app.get('/pets/:id', function (req, res) {
   fs.readFile(petsPath, 'utf8', function (err, petData) {
     if (err) {
       console.error(err.stack);
@@ -32,17 +37,24 @@ app.get('/pets:id', function (req, res) {
 
     let id = Number.parseInt(req.params.id);
     let pets = JSON.parse(petData);
-
-    // if (id < 0 || id >= pets.length || Number.isNaN(id)) {
-    //   return res.sendStatus(404);
-    // }
-    // res.set('Content-Type', 'text/plain');
-    // res.send(pets[id]);
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+    res.set('Content-Type', 'application/json');
+    res.send(pets[id]);
   });
 });
 
-
+app.post('/pets', function (req, res) {
+  if (err) {
+    console.error(err.stack);
+    return res.sendStatus(400);
+  }
+  res.send(JSON.stringify(req.body));
+})
 
 app.listen(port, function () {
   console.log('Listening on port', port);
 });
+
+module.exports = app;
